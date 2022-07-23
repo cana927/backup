2daae12d0a54289c01c213b9a83e8cd1e629110a
cana927 <limjulia927@gmail.com>
2022-07-22 23:22:04 -0700

    finished billboard2 usaco

--- u2015Dec/src/billboard2.java
+++ u2015Dec/src/billboard2.java
@@ -18,25 +18,31 @@ public class billboard2 {
 		int tx2 = Integer.parseInt(st.nextToken());
 		int ty2 = Integer.parseInt(st.nextToken());
 		
+		
 		int xOverlap = Math.min(x2, tx2)- Math.max(x1, tx1);
-		if (xOverlap==x2-x1) {
+		if (xOverlap>=x2-x1) {
 			xOverlap= 1;
+		} else if (xOverlap<0) {
+			xOverlap= 0;
 		}
 		int yOverlap = Math.min(y2, ty2)- Math.max(y1, ty1);
-		if (yOverlap==y2-y1) {
+		if (yOverlap>=y2-y1) {
 			yOverlap= 1;
+		}else if (yOverlap<0) {
+			xOverlap= 0;
 		}
 		int x = x2-x1;
 		int y = y2 -y1;
 		
-		if (yOverlap == 1) {
-			out.println ((x-xOverlap) * y);
+		if (xOverlap == 1 && yOverlap == 1) {
+			out.println (0);
+		}else if (tx1>x1 && tx2<x2 || ty1>y1 && ty2 < y2) {
+			out.println(x*y);
 		}
 		else if (xOverlap == 1) {
 			out.println((y-yOverlap) * x);
-			
-		} else if (xOverlap == 1 && yOverlap == 1) {
-			out.println (0);
+		} else if ( yOverlap == 1) {
+			out.println ((x-xOverlap) * y);
 		}else {
 			out.println (x*y);
 		}
